4d24b776569f247e576e619f019386e43bcae83b
lj <janluethi2000@gmail.com>
2023-03-30 07:49:09 +0200

    #183 add Lobby to skeleton

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/logic/lobby/Lobby.java
@@ -0,0 +1,63 @@
+package ch.uzh.ifi.hase.soprafs23.logic.lobby;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import ch.uzh.ifi.hase.soprafs23.logic.game.Game;
+import ch.uzh.ifi.hase.soprafs23.logic.role.Role;
+
+public class Lobby {
+    private String id;
+    private Player admin;
+    private List<Player> players;
+    private Map<Class<? extends Role>, Role> roles;
+    private boolean open;
+
+    public Lobby(String id, Player admin) {
+        this.id = id;
+        this.admin = admin;
+    }
+
+    public void addPlayer(Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addPlayer'");
+    }
+
+    public void removePlayer(Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removePlayer'");
+    }
+
+    public Collection<Role> getRoles() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRoles'");
+    }
+
+    public Collection<Role> getRolesOfPlayer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRolesOfPlayer'");
+    }
+
+    public Iterable<Player> getPlayersByRole(Class<? extends Role> roleClass) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPlayersByRole'");
+    }
+
+    public Iterable<Player> getAlivePlayers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAlivePlayers'");
+    }
+
+    public void addPlayerToRole(Player player, Class<? extends Role> role) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addPlayerToRole'");
+    }
+
+    public void instantiateRoles(Game game) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removePlayer'");
+    }
+
+    
+}
--- src/main/java/ch/uzh/ifi/hase/soprafs23/logic/lobby/Player.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/logic/lobby/Player.java
@@ -1,5 +1,37 @@
 package ch.uzh.ifi.hase.soprafs23.logic.lobby;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
-    
+    private String id;
+    private String name;
+    private boolean alive = true;
+    private List<PlayerObserver> observers = new ArrayList<>();
+
+    public Player(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void addObserver(PlayerObserver observer) {
+        observers.add(observer);
+    }
+
+    public void killPlayer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'killPlayer'");
+    }
 }
