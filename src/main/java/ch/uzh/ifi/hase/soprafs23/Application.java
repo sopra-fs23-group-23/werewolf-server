eedb19b7ac92fe42617ea4ef94f0af6ac9751200
Miroshima <miro.vannini@protonmail.ch>
2023-03-30 18:53:31 +0200

    first attempt to connect to locally hosted MySQL server

--- src/main/java/ch/uzh/ifi/hase/soprafs23/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/Application.java
@@ -9,12 +9,17 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import ch.uzh.ifi.hase.soprafs23.database.DBConnector;
+
 @RestController
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+
+    // TODO delete next line... was for connection testing only
+    DBConnector.connectDB();
   }
 
   @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
