bc3828e409ba37c33c65d28f95044cee33936f58
Miroshima <miro.vannini@protonmail.ch>
2023-04-01 09:47:57 +0200

    db connectoor

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/database/DBConnector.java
@@ -0,0 +1,34 @@
+package ch.uzh.ifi.hase.soprafs23.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBConnector {
+
+    public static void connectDB(){
+
+        try {
+
+            String connectionURL = "jdbc:mysql://localhost:3306/werewolves";
+            String username = "root";
+            String password = "8cnNNJ!YVi!F9h";
+
+            Connection connection = DriverManager.getConnection(connectionURL, username, password);
+
+            Statement statement = connection.createStatement();
+
+            // ResultSet resultSet = statement.executeQuery("insert into users (id, username, password)" + " values (2, 'eggmann', 'egg')");
+
+            ResultSet resultSet = statement.executeQuery("select * from users;");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("username"));
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
