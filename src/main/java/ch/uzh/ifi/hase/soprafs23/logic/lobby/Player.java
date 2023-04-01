f5fc69761025b2bc878f246054e9530dad54f7ab
lj <janluethi2000@gmail.com>
2023-04-01 10:49:09 +0200

    #42 make player comparable

--- src/main/java/ch/uzh/ifi/hase/soprafs23/logic/lobby/Player.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/logic/lobby/Player.java
@@ -40,4 +40,21 @@ public class Player {
             playerObserver.onPlayerKilled();
         }
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final Player other = (Player) obj;
+        return id == other.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
