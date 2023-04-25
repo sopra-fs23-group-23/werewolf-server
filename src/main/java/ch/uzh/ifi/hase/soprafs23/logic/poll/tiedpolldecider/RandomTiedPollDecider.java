6558a52d7b06612459e0e4ad2455d08d81838f3c
lj <janluethi2000@gmail.com>
2023-04-25 07:38:03 +0200

    #145 add RandomTiedPollDecider

--- src/main/java/ch/uzh/ifi/hase/soprafs23/logic/poll/tiedpolldecider/RandomTiedPollDecider.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/logic/poll/tiedpolldecider/RandomTiedPollDecider.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.hase.soprafs23.logic.poll.tiedpolldecider;
 
 import java.util.List;
+import java.util.Random;
 
 import ch.uzh.ifi.hase.soprafs23.logic.poll.Poll;
 import ch.uzh.ifi.hase.soprafs23.logic.poll.PollOption;
@@ -9,8 +10,9 @@ public class RandomTiedPollDecider implements TiedPollDecider {
 
     @Override
     public void executeTiePoll(Poll poll, List<PollOption> pollOptions, Runnable onTiePollFinished) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'executeTiePoll'");
+        PollOption selected = pollOptions.get(new Random().nextInt(pollOptions.size()));
+        poll.setResultCommand(selected.getPollCommand());
+        onTiePollFinished.run();
     }
     
 }
